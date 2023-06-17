0f4ca3c7f48e3873dbdddf4effbe1cbd13bf89b3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-17 10:17:59 -0300

    corrigindo o desafio do dia 1

--- dia_1/main.js
+++ dia_1/main.js
@@ -5,7 +5,7 @@ let stringTrinta = "30";
 let numeroDez = 10;
 let stringDez = "10";
 
-if (numeroUm === stringUm) {
+if (numeroUm == stringUm) {
 	console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
 } else {
 	console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
@@ -17,7 +17,7 @@ if (numeroTrinta === stringTrinta) {
 	console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo');
 }
 
-if (numeroDez === stringDez) {
+if (numeroDez == stringDez) {
 	console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
 } else {
 	console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
