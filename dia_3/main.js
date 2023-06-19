6d0f55b1c0014ec75f605ab09e246aa5b0f099e1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-18 23:05:10 -0300

    decisão entre tecnologias

--- dia_3/main.js
+++ dia_3/main.js
@@ -4,3 +4,9 @@ while (decisaoArea != 1 && decisaoArea != 2) {
 	alert("Escolha entre 1 ou 2!");
 	decisaoArea = parseInt(prompt("Deseja seguir pela área de Front-end ou Back-end?\n1 - Front-end ou 2 - Back-end"));
 }
+
+if (decisaoArea == 1) {
+	let decisaoTecnologia = parseInt(prompt("Deseja aprender React ou Vue?\n1 - React ou 2 - Vue"));
+} else if (decisaoArea == 2) {
+	let decisaoTecnologia = parseInt(prompt("Deseja aprender C# ou Java?\n1 - C# ou 2 - Java"));
+}
