40e801c5c613c9fb34596001d741e90189ae520b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-20 09:19:23 -0300

    objeto comida

--- dia_5/main.js
+++ dia_5/main.js
@@ -1,13 +1,49 @@
-let desejaAdicionarComida = parseInt(prompt("Deseja adicionar uma comida na sua lista de compras?\n1 - Sim ou 2 - Não"));
+let listaDeCompras = [];
 
-while (desejaAdicionarComida != 1 && desejaAdicionarComida != 2) {
-	alert("Digite 1 ou 2!!");
-	desejaAdicionarComida = prompt("Deseja adicionar uma comida na sua lista de compras?\n1 - Sim ou 2 - Não");
-}
+while (true) {
+	let desejaAdicionarComida = parseInt(prompt("Deseja adicionar uma comida na sua lista de compras?\n1 - Sim ou 2 - Não"));
 
-let comidaDesejada = prompt("Qual comida você deseja inserir?");
+	if (desejaAdicionarComida == 2) {
+		break;
+	}
 
-while (comidaDesejada.length == 0) {
-	alert("Digite a comida desejada!!");
-	comidaDesejada = prompt("Qual comida você deseja inserir?");
+	while (desejaAdicionarComida != 1 && desejaAdicionarComida != 2) {
+		alert("Digite 1 ou 2!!");
+		desejaAdicionarComida = prompt("Deseja adicionar uma comida na sua lista de compras?\n1 - Sim ou 2 - Não");
+	}
+
+	let comidaDesejada = prompt("Qual comida você deseja inserir?");
+
+	while (comidaDesejada.length == 0) {
+		alert("Digite a comida desejada!!");
+		comidaDesejada = prompt("Qual comida você deseja inserir?");
+	}
+
+	let categoriaComida = parseInt(prompt("Qual categoria a/o " + comidaDesejada + " se encaixa?\n1 - Frutas, 2 - Laticínios, 3 - Congelados, 4 - Doces."));
+	let categoria;
+
+	switch (categoriaComida) {
+		case 1:
+			categoria = "Frutas";
+		break;
+
+		case 2:
+			categoria = "Laticínios";
+		break;
+
+		case 3:
+			categoria = "Congelados";
+		break;
+
+		case 4:
+			categoria = "Doces";
+		break;
+	}
+
+	let comida = {
+		nome : comidaDesejada,
+		categoria : categoria
+	}
+
+	listaDeCompras.push(comida);
 }
