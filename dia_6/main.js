6a8e75f050b74d5db9c31d81d69cdee805022d7e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-21 16:04:41 -0300

    lógica para excluir comida da lista

---
+++ dia_6/main.js
@@ -0,0 +1,77 @@
+let listaDeCompras = [];
+let comida;
+
+while (true) {
+	let desejaAdicionarComida = parseInt(prompt("Deseja adicionar uma comida na sua lista de compras?\n1 - Sim ou 2 - Não"));
+
+	if (desejaAdicionarComida == 2) {
+		break;
+	}
+
+	while (desejaAdicionarComida != 1 && desejaAdicionarComida != 2) {
+		alert("Digite 1 ou 2!!");
+		desejaAdicionarComida = prompt("Deseja adicionar uma comida na sua lista de compras?\n1 - Sim ou 2 - Não");
+	}
+
+	let comidaDesejada = prompt("Qual comida você deseja inserir?");
+
+	while (comidaDesejada.length == 0) {
+		alert("Digite a comida desejada!!");
+		comidaDesejada = prompt("Qual comida você deseja inserir?");
+	}
+
+
+	let categoriaComida = parseInt(prompt("Qual categoria a/o " + comidaDesejada + " se encaixa?\n1 - Frutas, 2 - Laticínios, 3 - Congelados, 4 - Doces."));
+	let categoria;
+
+	switch (categoriaComida) {
+		case 1:
+			categoria = "Frutas";
+		break;
+
+		case 2:
+			categoria = "Laticínios";
+		break;
+
+		case 3:
+			categoria = "Congelados";
+		break;
+
+		case 4:
+			categoria = "Doces";
+		break;
+	}
+
+	comida = {
+		nomeComida : comidaDesejada,
+		categoriaComida : categoria
+	}
+
+	listaDeCompras.push(comida);
+
+	if (listaDeCompras.length > 0) {
+		let desejaRemoverComida = parseInt(prompt("Deseja remover alguma comida da lista?\n1 - Sim ou 2 - Não"));
+
+		if (desejaRemoverComida == 1) {
+			let removeComida = prompt("Digite o nome da comida:");
+
+			for (let i = 0; i < listaDeCompras.length; i++) {
+				if (listaDeCompras[i].nomeComida == removeComida) {
+					listaDeCompras.splice(i, 1);
+				}
+			}
+		}
+	}
+}
+
+for (let i = 0; i < listaDeCompras.length; i++) {
+	console.log(listaDeCompras[i]);
+}
+
+document.write("<br>Lista de compras:");
+/*
+document.write("<br>&nbsp;&nbsp;&nbsp;Frutas: " + frutas);
+document.write("<br>&nbsp;&nbsp;&nbsp;Laticínios: " + laticinios);
+document.write("<br>&nbsp;&nbsp;&nbsp;Congelados: " + congelados);
+document.write("<br>&nbsp;&nbsp;&nbsp;Doces: " + doces);
+*/
