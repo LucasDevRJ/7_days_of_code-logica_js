266afecf40bcdcdc6b1771f8beb8d099df0c9688
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-21 17:03:13 -0300

    lógica de add e remover comida da lista finalizada

--- dia_6/main.js
+++ dia_6/main.js
@@ -20,7 +20,6 @@ while (true) {
 		comidaDesejada = prompt("Qual comida você deseja inserir?");
 	}
 
-
 	let categoriaComida = parseInt(prompt("Qual categoria a/o " + comidaDesejada + " se encaixa?\n1 - Frutas, 2 - Laticínios, 3 - Congelados, 4 - Doces."));
 	let categoria;
 
@@ -49,8 +48,15 @@ while (true) {
 
 	listaDeCompras.push(comida);
 
+	alert(comida.nomeComida + " - " + comida.categoriaComida + " adicionado com sucesso!");
+
 	if (listaDeCompras.length > 0) {
 		let desejaRemoverComida = parseInt(prompt("Deseja remover alguma comida da lista?\n1 - Sim ou 2 - Não"));
+
+		while (desejaRemoverComida != 1 && desejaRemoverComida != 2) {
+			alert("Digite somente 1 ou 2!!");
+			desejaRemoverComida = parseInt(prompt("Deseja remover alguma comida da lista?\n1 - Sim ou 2 - Não"));
+		}
 
 		if (desejaRemoverComida == 1) {
 			let removeComida = prompt("Digite o nome da comida:");
@@ -58,20 +64,32 @@ while (true) {
 			for (let i = 0; i < listaDeCompras.length; i++) {
 				if (listaDeCompras[i].nomeComida == removeComida) {
 					listaDeCompras.splice(i, 1);
-				}
+					alert(removeComida + " removido com sucesso!");
+				} 
 			}
 		}
 	}
 }
 
+let frutas = "";
+let laticinios = "";
+let congelados = "";
+let doces = "";
+
 for (let i = 0; i < listaDeCompras.length; i++) {
-	console.log(listaDeCompras[i]);
+	if (listaDeCompras[i].categoriaComida == "Frutas") {
+		frutas += listaDeCompras[i].nomeComida + ", ";
+	} else if (listaDeCompras[i].categoriaComida == "Laticínios") {
+		laticinios += listaDeCompras[i].nomeComida + ", ";
+	} else if (listaDeCompras[i].categoriaComida == "Congelados") {
+		congelados += listaDeCompras[i].nomeComida + ", ";
+	} else if (listaDeCompras[i].categoriaComida == "Doces") {
+		doces += listaDeCompras[i].nomeComida + ", ";
+	}
 }
 
 document.write("<br>Lista de compras:");
-/*
 document.write("<br>&nbsp;&nbsp;&nbsp;Frutas: " + frutas);
 document.write("<br>&nbsp;&nbsp;&nbsp;Laticínios: " + laticinios);
 document.write("<br>&nbsp;&nbsp;&nbsp;Congelados: " + congelados);
 document.write("<br>&nbsp;&nbsp;&nbsp;Doces: " + doces);
-*/
